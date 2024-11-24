06801624a40ad624cc4bf10ee55df480b7b1249b
Nooruddin Md Afsar <nooruddin.mdafsar.it25@heritageit.edu.in>
2024-11-24 23:10:24 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
+export default defineConfig({
+  plugins: [react()],
+  preview: {
+    port: parseInt(process.env.PORT || '3000', 10), // Use the Render-specified port
+    host: '0.0.0.0', // Ensure external access
+  },
+});
   
   return {
     plugins: [react()],
